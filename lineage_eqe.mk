1fef1688c0cac1ad7b11440fa77a93fd453c3174
Kalpu-24 <22amtics013@gmail.com>
2024-10-08 22:53:08 +0530

    proper conf

--- lineage_eqe.mk
+++ lineage_eqe.mk
@@ -18,14 +18,15 @@
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from berlin device
-$(call inherit-product, device/motorola/eqe/device.mk)
+#TODO
+# Inherit from eqe device
+#$(call inherit-product, device/motorola/eqe/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_eqe
-PRODUCT_DEVICE := eqe_g
+PRODUCT_DEVICE := eqe
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := motorola edge 50 pro
@@ -36,4 +37,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=eqe_g_retail \
     PRIVATE_BUILD_DESC="eqe_g-user 14 U2UMS34.27-38-1-2-3-1 76e47-863bd0 release-keys MU-134"
 
-BUILD_FINGERPRINT := motorola/eqe_g/eqe:14/U2UMS34.27-38-1-2-3-1/76e47-863bd0:user/release-keys
+BUILD_FINGERPRINT := motorola/eqe_g/eqe:13/U2UMS34.27-38-1-2-3-1/76e47:user/release-keys
