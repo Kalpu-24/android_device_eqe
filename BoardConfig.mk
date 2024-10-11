164b4da40ad763bdc82466d784831ddc51bd03ee
Kalp Shah <22amtics013@gmail.com>
2024-10-11 15:56:55 +0530

    Define Bootloader and Platform flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,5 +15,13 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a510
 
+#Bootloader flags
+TARGET_BOOTLOADER_BOARD_NAME := eqe
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := sm7550-ab
+
 # Inherit the proprietary files
 include vendor/motorola/eqe/BoardConfigVendor.mk
