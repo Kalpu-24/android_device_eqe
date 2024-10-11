e28011da07a6cf1f2b2888b4df9948722a5e9ecf
Kalp Shah <22amtics013@gmail.com>
2024-10-11 15:47:28 +0530

    Define Arch flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,19 @@
 DEVICE_PATH := device/motorola/eqe
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8.4-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a715
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8.2-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a510
+
 # Inherit the proprietary files
 include vendor/motorola/eqe/BoardConfigVendor.mk
